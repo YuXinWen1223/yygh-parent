8c49ee32db922d16038a40cf9e503bdf8c0c655c
余欣文 <1835190225@qq.com>
2022-05-19 09:22:09 +0800

    第二天统一日志处理，医院后接口实现

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controlle/HospitalSetController.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controlle/HospitalSetController.java
@@ -1,15 +1,20 @@
 package com.atguigu.yygh.hosp.controlle;
 
+import com.atguigu.yygh.common.result.Result;
+import com.atguigu.yygh.common.utils.MD5;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author : 余欣文
@@ -25,11 +30,137 @@ public class HospitalSetController {
 
     @ApiOperation(value = "获取所有医院设置")
     @GetMapping("findAll")
-    public Object findAllHospitalSet() {
+    public Result findAllHospitalSet() {
         //调用service的方法
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok();
     }
 
+    /**
+     * 多条件分页查询
+     *
+     * @param current
+     * @param limit
+     * @param hospitalSetQueryVo
+     * @return
+     */
+    @ApiOperation(value = "多条件分页查询医院")
+    @PostMapping("findPageHospSet/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        Page<HospitalSet> page = new Page<>(current, limit);
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        //医院名称
+        String hosname = hospitalSetQueryVo.getHosname();
+        //医院编号
+        String hoscode = hospitalSetQueryVo.getHoscode();
+        if (!StringUtils.isEmpty(hosname)) wrapper.like("hosname", hospitalSetQueryVo.getHosname());
+        if (!StringUtils.isEmpty(hoscode)) wrapper.eq("hoscode", hospitalSetQueryVo.getHoscode());
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, wrapper);
+        long total = page.getTotal();
+        return Result.ok(pageHospitalSet);
+    }
 
+    /**
+     * 4 添加医院设置
+     *
+     * @param hospitalSet
+     * @return
+     */
+    @ApiOperation(value = "添加医院设置")
+    @PostMapping("saveHospitalSet")
+    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet) {
+        //设置状态 1 使用 0 不能使用
+        hospitalSet.setStatus(1);
+        //签名秘钥
+        Random random = new Random();
+        hospitalSet.setSignKey(MD5.encrypt(System.currentTimeMillis() + "" + random.nextInt(1000)));
+        //调用service
+        boolean save = hospitalSetService.save(hospitalSet);
+        if (save) return Result.ok();
+        else return Result.fail();
+    }
+
+    /**
+     * 5 根据id获取医院设置
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "根据id获取医院设置")
+    @GetMapping("getHospSet/{id}")
+    public Result getHospSet(@PathVariable Long id) {
+//        try {
+//            //模拟异常
+//            int a = 1/0;
+//        }catch (Exception e) {
+//            throw new YyghException("失败",201);
+//        }
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok(hospitalSet);
+    }
+
+    /**
+     * 6 修改医院设置
+     *
+     * @param hospitalSet
+     * @return
+     */
+    @ApiOperation(value = "修改医院设置")
+    @PostMapping("updateHospitalSet")
+    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet) {
+        boolean flag = hospitalSetService.updateById(hospitalSet);
+        if (flag) return Result.ok();
+        else return Result.fail();
+    }
+
+    /**
+     * 7 批量删除医院设置
+     *
+     * @param idList
+     * @return
+     */
+    @ApiOperation(value = "批量删除医院设置")
+    @DeleteMapping("batchRemove")
+    public Result batchRemoveHospitalSet(@RequestBody List<Long> idList) {
+        hospitalSetService.removeByIds(idList);
+        return Result.ok();
+    }
+
+    /**
+     * 8 医院设置锁定和解锁
+     *
+     * @param id
+     * @param status
+     * @return
+     */
+    @ApiOperation(value = "医院设置锁定和解锁")
+    @PutMapping("lockHospitalSet/{id}/{status}")
+    public Result lockHospitalSet(@PathVariable Long id,
+                                  @PathVariable Integer status) {
+        //根据id查询医院设置信息
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        //设置状态
+        hospitalSet.setStatus(status);
+        //调用方法
+        hospitalSetService.updateById(hospitalSet);
+        return Result.ok();
+    }
+
+    /**
+     * 9 发送签名秘钥
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "发送签名秘钥")
+    @PutMapping("sendKey/{id}")
+    public Result lockHospitalSet(@PathVariable Long id) {
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        String signKey = hospitalSet.getSignKey();
+        String hoscode = hospitalSet.getHoscode();
+        //TODO 发送短信
+        return Result.ok();
+    }
 }
